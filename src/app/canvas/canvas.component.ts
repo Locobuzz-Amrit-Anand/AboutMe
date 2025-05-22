0200ebbd55c1b7eb8366b522e215d30b2da72796
Amrit-Locobuzz-Anand <168397393+Locobuzz-Amrit-Anand@users.noreply.github.com>
2025-05-22 10:37:06 +0530

    Add: Data entered

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { ContentComponent } from "../content/content.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-canvas',
   standalone: true, 
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ContentComponent, FooterComponent],
   templateUrl: './canvas.component.html',
   styleUrl: './canvas.component.css'
 })
